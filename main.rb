c3c0fe677a54bbcd49468a7a6318757be49de19e
Nathan Lunn <nathandouglaslunn@gmail.com>
2022-05-30 14:54:37 +0000

    add Round class

--- main.rb
+++ main.rb
@@ -8,4 +8,14 @@ class Player
 
 end
 
+class Round
+  attr_accessor :player, :question, :answer
 
+  def initialize(player)
+    @player = player
+    first_num = (rand() * 20).to_i
+    second_num = (rand() * 20).to_i
+    @question = "what is #{first_num} plus #{second_num}"
+    @answer = first_num + second_num
+  end
+end
